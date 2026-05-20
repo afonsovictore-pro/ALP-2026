21c5603e099de08dc0c2416ab0e4037f5bc766b3
afonsovictore-pro <afonsovictore@gmail.com>
2026-05-20 14:59:03 -0300

    Create 2b.py

---
+++ lab 7/2b.py
@@ -0,0 +1,9 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+soma = 0
+contador = 0 #nao tinha contador
+while contador <= 10: 
+    num = int(input("Digite um número para somar: "))
+    soma += num # nao tinha print
+    print (soma)
